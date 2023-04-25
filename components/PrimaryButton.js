8544e071baa49863c1efe295ef6627f37557cd2e
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-25 20:06:00 +0300

    Pressable, ripple effect and other styling

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,43 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log('pressed');
+  }
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.pressableContainer, styles.pressed]
+            : styles.pressableContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: '640233' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: '#72063c',
+    borderRadius: 28,
+    margin: 4,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressableContainer: {
+    width: '100%',
+    paddingVertical: 8,
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
