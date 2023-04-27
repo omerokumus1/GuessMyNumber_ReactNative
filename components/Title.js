48f2658d9449c2570fa4d4403faadbc443f64ce7
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-27 20:27:31 +0300

    Title Component

---
+++ components/Title.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text } from 'react-native';
+import Colors from '../constants/Colors';
+
+function StyledTitle({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+export default StyledTitle;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.accent500,
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: Colors.accent500,
+    padding: 24,
+  },
+});
