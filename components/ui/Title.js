b2299743d87acd44fe2a41f147bc2cd6b6b2f598
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-27 20:41:51 +0300

    file organization

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text } from 'react-native';
-import Colors from '../constants/Colors';
+import Colors from '../../constants/Colors';
 
 function StyledTitle({ children }) {
   return <Text style={styles.title}>{children}</Text>;
