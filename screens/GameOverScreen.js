1ac0de46a8b473fe7f62620c677c133fcbcc9f8c
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-30 10:23:57 +0300

    design and styling of GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,41 @@
+import { StyleSheet, View, Image } from 'react-native';
 import Title from '../components/ui/Title';
+import Colors from '../constants/Colors';
 
 function GameOverScreen() {
-  return <Title>Game Over</Title>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>Game Over</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/success.png')}
+        />
+      </View>
+    </View>
+  );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    margin: 36,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
